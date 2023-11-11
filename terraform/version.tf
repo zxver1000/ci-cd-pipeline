257735ba25b1403d5672987b730d59ddebb3515a
Eugene <65178775+dev-whoan@users.noreply.github.com>
2023-11-11 16:39:58 +0900

    Update terraform versions

--- terraform/version.tf
+++ terraform/version.tf
@@ -4,6 +4,18 @@ locals {
     }
 }
 
+# 필요한 provider가 있다면 적절히 추가하여 사용합니다.
+terraform {
+    required_version = "1.5.7"
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "5.17.0"
+        }
+    }
+}
+
 provider "aws" {
     region = "ap-northeast-2"
 
